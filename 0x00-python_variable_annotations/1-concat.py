7ac011c323dea24c633de7b516e3e8b668ddf8d0
Sandra Calero <3358@holbertonschool.com>
2022-06-17 16:23:02 -0500

    ADD a type-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+"Returning the concatenation of str1 and str2."
+The first line of the docstring is a one sentence summary of the function
+:param str1: str
+:type str1: str
+:param str2: str
+:type str2: str
+:return: The concatenation of str1 and str2.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Returning the concatenation of str1 and str2."""
+    return str1 + str2
